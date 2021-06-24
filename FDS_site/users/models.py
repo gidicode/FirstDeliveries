ebc94b34f51bed892a09d8c2af62181f2fc011f0
GIDEON UGWO <usuugwo@gmail.com>
2021-06-24 21:39:54 +0100

    wuth dashboard Controls

--- FDS_site/users/models.py
+++ FDS_site/users/models.py
@@ -102,10 +102,11 @@ class MakeRequest(models.Model):
     date_created = models.DateTimeField(default=timezone.now, null=True)
     status = models.CharField(max_length=20, choices=STATUS, default='Pending', null=True)
     Loading_choice = models.CharField(max_length=100, null=True)
-    Amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    Amount_paid = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     Amount_Payable = models.IntegerField(null= True, default=0, )
     charge_id = models.CharField(max_length=100, null=True, validators=[alphanumeric])
     paid = models.BooleanField(default=False)
+    confirmed = models.BooleanField(default=False)
     order_id= models.CharField(max_length=10, null=True, default=0)
     assigned = models.BooleanField(default=False)
 
@@ -174,6 +175,7 @@ class MakeRequestCash(models.Model):
     status = models.CharField(max_length=20, choices=STATUS, default='Pending', null=True)
     Loading_choice = models.CharField(max_length=100, null=True)
     paid = models.BooleanField(default=False)
+    confirmed = models.BooleanField(default=False)
     Amount_Paid = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     Amount_Payable = models.IntegerField(null= True, default=0, )
     order_id= models.CharField(max_length=10, null=True, default=0)
@@ -209,6 +211,7 @@ class Anonymous(models.Model):
     date_created = models.DateTimeField(default=timezone.now, null=True)
     status = models.CharField(max_length=20, choices=STATUS, default='Pending', null=True)
     paid = models.BooleanField(default=False)
+    confirmed = models.BooleanField(default=False)
     email = models.EmailField(max_length=100, null=True)
     Amount_Paid = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     Amount_Payable = models.DecimalField(max_digits=10, decimal_places=2, default=0)
@@ -256,7 +259,7 @@ class Shopping(models.Model):
     Amount_Refunded = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     order_id= models.CharField(max_length=10, null=True, default=0)
     assigned = models.BooleanField(default=False)
-
+    confirmed = models.BooleanField(default=False)
     
     def __str__(self):
         return f'Customer:{self.customer}, Order ID:{self.order_id}'
@@ -296,38 +299,43 @@ class Errand_service(models.Model):
 
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
     category = models.CharField(max_length = 100, null=True, choices=category_choice)
-    fuel_per_amount = models.IntegerField(null=True, verbose_name='Petrol Amount', help_text='25Ltrs max per order (N163/ltr, N4075-25ltrs')
-    payment_channel = models.CharField(max_length=100, choices=payment_choice, null=True, verbose_name='Payment Choice')
-    Bread_brand_name = models.CharField(max_length=100, null=True, blank=True, verbose_name='Bread Name')
-    Quantity = models.CharField(max_length=100, null=True, blank=True)
-    description = models.CharField(max_length=100, null=True, blank=True, help_text="Further description")
-    Enter_amount = models.IntegerField(null=True, verbose_name='Amount', help_text='Cost to purchase items')
-    Drug_store = models.CharField(max_length = 100, null= True, verbose_name="Pharmacy Name", help_text="Write 'None' if none")
-    Drug_name = models.CharField(max_length = 200, null=True, help_text='Must be an over the counter drug')
+    fuel_per_amount = models.IntegerField(null=True,default=0, verbose_name='Petrol Amount', help_text='25Ltrs max per order (N163/ltr, N4075-25ltrs')
+    payment_channel = models.CharField(max_length=100, default=0, choices=payment_choice, null=True, verbose_name='Payment Choice')
+    Bread_brand_name = models.CharField(max_length=100, default=0, null=True, blank=True, verbose_name='Bread Name')
+    Quantity = models.CharField(max_length=100, null=True, default=0, blank=True)
+    description = models.CharField(max_length=100, null=True, default=0, blank=True, help_text="Further description")
+    Enter_amount = models.IntegerField(null=True, verbose_name='Amount',  default=0,help_text='Cost to purchase items')
+    Drug_store = models.CharField(max_length = 100, null= True, default=0, verbose_name="Pharmacy Name", help_text="Write 'None' if none")
+    Drug_name = models.CharField(max_length = 200, null=True,  default=0, help_text='Must be an over the counter drug')
     Gas_Quantity = models.IntegerField(null=True, help_text='Enter the quantity of gas you want refilled (1-12kg)')
-    Shawarma_store = models.CharField(max_length=100, null=True, blank=True,)
-    Shawarma_desc = models.CharField(max_length=100, null=True, choices=Shawarma_type, verbose_name='Preference')
-    pizza_store = models.CharField(max_length=100, null=True, verbose_name='Place of Purchase')
-    Pizza_desc = models.CharField(max_length=100, null=True, verbose_name='Type of Pizza')
-    ice_Cream_desc = models.CharField(max_length=100, null=True, verbose_name='Ice Cream Description')
-    ice_Cream_store = models.CharField(max_length=100, null=True)
-    fruits_description = models.CharField(max_length=100, null=True)
-    fruits_purchase_store = models.CharField(max_length=100, null=True, blank=True, verbose_name='Purchase Location')
+    Shawarma_store = models.CharField(max_length=100, default=0, null=True, blank=True,)
+    Shawarma_desc = models.CharField(max_length=100, null=True, default=0, choices=Shawarma_type, verbose_name='Preference')
+    pizza_store = models.CharField(max_length=100, null=True, default=0, verbose_name='Place of Purchase')
+    Pizza_desc = models.CharField(max_length=100, null=True, default=0, verbose_name='Type of Pizza')
+    ice_Cream_desc = models.CharField(max_length=100, null=True, default=0, verbose_name='Ice Cream Description')
+    ice_Cream_store = models.CharField(max_length=100, default=0, null=True)
+    fruits_description = models.CharField(max_length=100, default=0, null=True)
+    fruits_purchase_store = models.CharField(max_length=100, default=0, null=True, blank=True, verbose_name='Purchase Location')
     medical_prescription = models.FileField(upload_to='medical_presciption', default= "default.jpg", null=True, blank=True, help_text='Alternatively upload precription')
-    Food_Vendor = models.CharField(max_length=100, null=True, )
-    Food_description = models.CharField(max_length=200, null=True)
+    Food_Vendor = models.CharField(max_length=100, null=True, default=0, )
+    Food_description = models.CharField(max_length=200, null=True, default=0,)
     your_location = models.CharField(max_length=100, null=True, blank=True)
-    order_id = models.CharField(max_length=7, null=True)
-    Ps_reference = models.CharField(max_length=10, null=True, )
+    order_id = models.CharField(max_length=7, null=True, default=0,)
+    Ps_reference = models.CharField(max_length=10, null=True, default=0, )
     date_created = models.DateTimeField(default=timezone.now, null=True)
     assigned = models.BooleanField(default=False)
     paid = models.BooleanField(default=False)
+    confirmed = models.BooleanField(default=False)
+    profit = models.IntegerField(null= True, default=0)
     Amount_Paid = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     Amount_Payable = models.IntegerField(null= True, default=0, )
     status = models.CharField(max_length=100, choices=STATUS, default='Pending', null=True)
 
     def __str__(self):
         return f' Customer:{self.customer}, Order ID:{self.order_id}, Category:{self.category}'
+
+    class Meta:
+        ordering = ('-date_created',)
     
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs) 
@@ -351,21 +359,45 @@ class Front_desk(models.Model):
         ('multiple', 'multiple'),
     ]
     delivery_type = [
-        ('Pick & Drop', 'Pick & Drop')
+        ('Pick & Drop', 'Pick & Drop'),
         ('Errand', 'Errand')
     ]
+
+    STATUS = {
+        ('Pending', 'Pending'),
+        ('Canceled', 'Canceled'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    }
+
+    class Meta:
+        ordering = ('-date_created',)
+
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$')
 
     customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
     customer_name = models.CharField(max_length=100, null=True, blank=True)
     item_description = models.CharField(max_length=200, null=True, blank=True)
     customer_location = models.CharField(max_length=100, null=True, blank=True)
     delivery_destination = models.CharField(max_length=200, null=True, blank=True)
+    Reciever_phone_number = models.CharField(validators=[phone_regex], max_length=17, null=True, blank=True, verbose_name="Reciever Number")
+    Customer_phone_number = models.CharField(validators=[phone_regex], max_length=17, null=True, verbose_name="Customer Phone Number")
     Choice_for_TP = models.CharField(max_length=20, choices=OPTIONS2, null=True)
-    Delivery_type = models.CharField(max_length=20, choices=TYPE, null=True)
+    Delivery_type = models.CharField(max_length=20, choices=delivery_type, null=True)
+    Purchase_location = models.CharField(max_length=20,blank=True, null=True)
     Quantity = models.IntegerField(null=True, blank=True)
-    Enter_amount = models.IntegerField(null=True, verbose_name='Amount', help_text='Cost to purchase items')
-    
-
+    Enter_amount = models.IntegerField(null=True, verbose_name='Item Amount', help_text='Cost to purchase items')
+    Note = models.CharField(max_length=20, null=True, blank="True")
+    date_created = models.DateTimeField(default=timezone.now, null=True)
+    order_id = models.CharField(max_length=7, null=True)
+    assigned = models.BooleanField(default=False)
+    status = models.CharField(max_length=20, choices=STATUS, default='Pending', null=True)
+    payments_confirmed = models.BooleanField(default=False)
+    paid = models.BooleanField(default=False)
+    confirmed = models.BooleanField(default=False)
+    profit = models.IntegerField(null= True, default=0)
+    Amount_Paid = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    Amount_Payable = models.IntegerField(null= True, default=0)
 
 class ForPayments(models.Model):
     OPTION2 = {
