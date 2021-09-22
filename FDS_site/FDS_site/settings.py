db19f11f3524903aabc866975dfbbe5226b454e2
GIDEON UGWO <usuugwo@gmail.com>
2021-09-22 14:17:26 +0100

    Changed UI report system

--- FDS_site/FDS_site/settings.py
+++ FDS_site/FDS_site/settings.py
@@ -198,19 +198,19 @@ TINYMCE_JS_URL = STATIC_URL + 'tinymce/tinymce.min.js'
 
 TINYMCE_JS_ROOT = STATIC_ROOT + 'tinymce'
 
-TINYMCE_DEFAULT_CONFIG = {
+TINYMCE_DEFAULT_CONFIG = { 
     "height": "320px",
-    "width": "960px",
+    "width": "auto",
     "menubar": "file edit view insert format tools table help",
     "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
-    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "fullscreen insertdatetime media table paste code wordcount spellchecker",
     "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
     "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
     "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
     "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
     "a11ycheck ltr rtl | showcomments addcomment code",
     "custom_undo_redo_levels": 10,
-    "language": "es_ES",
+    "language": "en-us",
 }
 
 TINYMCE_SPELLCHECKER = True
