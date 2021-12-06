c51d60da95c8e3af6f9a3c71e0911c603f271aa7
GIDEON UGWO <usuugwo@gmail.com>
2021-12-06 12:40:38 +0100

    vue js frontend

--- FDS_site/frontend/src/router.js
+++ FDS_site/frontend/src/router.js
@@ -4,3 +4,7 @@ import VueRouter from 'vue-router'
 import allFleet from '@/components/allFleet'
 import AllRiders from '@/components/AllRiders'
 import AllCashRequest from '@/components/AllCashRequest'
+import AllErrandService from '@/components/AllErrandService'
+import AllFrontDesk from '@/components/AllFrontDesk'
+import AllShopping from '@/components/AllShopping'
+import 
